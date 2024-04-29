6d7e9fd9918507208531da341319f7383c5abbc0
pauldiegoaguilar <diego.paul.et.n26@gmail.com>
2024-04-29 16:47:07 -0300

    index

--- Lautaro Porcel Sabino 4°15°/functionejercicio4().js
+++ Lautaro Porcel Sabino 4°15°/functionejercicio4().js
@@ -1,4 +1,25 @@
 function ejercicio4(){
+    var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
+        'E', 'T'];
 
-    
+    let dni = document.querySelector("#dni").value;
+    let letrauser = document
+ 
+
+
+    if(dni > 0 && dni < 99999999) {
+    let nletra = dni % 23;
+
+    let letra = letras[nletra];
+
+    if(letra == letrauser){
+        console.log("Las letras son iguales")
+   
+
+        console.log("La letra de tu DNI es:  "+ letra);
+   
+    }
+
+    }
 }
+       
