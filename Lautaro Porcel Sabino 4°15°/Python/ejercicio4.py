c2315e18b727c273f2774e8a023abadd03cc5dd6
LautaroPorcel <164916015+LautaroPorcel@users.noreply.github.com>
2024-05-13 16:46:07 -0300

    ejercicio4

---
+++ Lautaro Porcel Sabino 4°15°/Python/ejercicio4.py
@@ -0,0 +1,8 @@
+#Ejercicio 4
+#Dado 1 número con decimales, que representa el lado de un cuadrado se pide informar la superficie del mismo.
+
+def multiplicacion():
+    Cuadrado = float(input("Ingrese un valor decimal"))
+    dato = Cuadrado * 4
+    
+    print("El area del triangulo es:  ", dato)
