62f0f04e7c540ebaf91ac17f6c36a795cad06730
LautaroPorcel <164916015+LautaroPorcel@users.noreply.github.com>
2024-05-13 16:45:56 -0300

    ejercicio3

---
+++ Lautaro Porcel Sabino 4°15°/Python/ejercicio3.py
@@ -0,0 +1,10 @@
+#Ejercicio 3
+#Dados 2 números enteros, que representan los lados de un rectángulo se pide informar la superficie del mismo.
+
+def multiplicacion ():
+    Valor1 = int(input("Ingrese el valor de la superficie"))
+    valor2 = int(input("Ingrese el valor de la superficie"))
+    Solucion = (Valor1 * valor2)
+    
+    print("La altura es   ", Solucion)
+    
