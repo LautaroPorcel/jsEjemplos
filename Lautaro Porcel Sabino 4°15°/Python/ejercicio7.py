a10e823e96a5e8dceb056cd3038cc43ebd5979b0
LautaroPorcel <164916015+LautaroPorcel@users.noreply.github.com>
2024-05-13 16:46:23 -0300

    ejercicio7

---
+++ Lautaro Porcel Sabino 4°15°/Python/ejercicio7.py
@@ -0,0 +1,13 @@
+#Ejercicio7
+#Dados 6 números reales, informar el promedio de los mismos.
+
+def ejercicio7():
+   N1 = float(input("el valor es: "))
+   N2 = float(input("el valor es: "))
+   N3 = float(input("el valor es: "))
+   N4 = float(input("el valor es: ")) 
+   N5 = float(input("el valor es: "))
+   N6 = float(input("el valor es: "))
+
+   resultado = (N1 + N2 + N3 + N4 + N5 + N6)/6
+   print("el promedio es:  ", resultado)
