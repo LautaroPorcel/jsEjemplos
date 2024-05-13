6578558683a7d59762e8981697f88244ab36c4fb
LautaroPorcel <164916015+LautaroPorcel@users.noreply.github.com>
2024-05-13 16:46:28 -0300

    ejercicio8

---
+++ Lautaro Porcel Sabino 4°15°/Python/ejercicio8.py
@@ -0,0 +1,10 @@
+#Ejercicio8
+#Dados 2 números enteros, que representan una cantidad parcial y total se pide: Calcular e informar 
+#el porcentaje que representa la primera de la segunda. ¿Qué tipo de datos son los recomendados para este algoritmo?
+
+def ejercicio8():
+    n1 = int(input("Ingrese un valor numerico"))
+    n2 = int(input("Ingrese un valor numerico"))
+    resultado = (n1 * n2)/100
+
+    print("El promedio es:  ", resultado)
