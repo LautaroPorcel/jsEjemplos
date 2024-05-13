46707dce72a8b8c9cba09b49b568a4a3d4037801
LautaroPorcel <164916015+LautaroPorcel@users.noreply.github.com>
2024-05-13 16:45:47 -0300

    ejercicio2

---
+++ Lautaro Porcel Sabino 4°15°/Python/ejercicio2.py
@@ -0,0 +1,11 @@
+#Ejercicio 2    
+#Leer 4 números enteros, calcular la suma e informar el resultado.
+n1 = int(input("Ingrese un valor"))
+n2 = int(input("Ingrese un valor"))
+n3 = int(input("Ingrese un valor"))
+n4 = int(input("Ingrese un valor"))
+result = int(input(n1 + n2 + n3 + n4))
+
+if n1 + n2 + n3 + n4:
+        print("El resultado es   "+ result)
+  
