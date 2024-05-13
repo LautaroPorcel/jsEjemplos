9a6016c2f6680a28e35391326b09817ee92815e7
LautaroPorcel <164916015+LautaroPorcel@users.noreply.github.com>
2024-05-13 16:45:35 -0300

    ejercicio1

---
+++ Lautaro Porcel Sabino 4°15°/Python/ejercicio1.py
@@ -0,0 +1,9 @@
+#Ejercicio 1
+#Leer 2 números enteros, calcular la suma e informar el resultado.
+def ejercicio1p():
+    N1 = int(input("Ingrese un numero"))
+    N2 = int(input("Ingrese un numero"))
+    resultado = int(input(N1 + N2))    
+
+    if N1 + N2:
+        print("el resultado es  "+ resultado)
